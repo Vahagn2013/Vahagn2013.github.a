4f3c2ba86d013cd5f9ab76f5b37d3e129e1223c6
Vahagn2013 <151218169+Vahagn2013@users.noreply.github.com>
2023-11-17 22:08:47 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// xndir 1
+
+// let object = {
+//     name: `Karen`,
+//     surname: `Abovyan`,
+//     profession: `Developer`,
+//     age: 24,
+//     salary: 420000,
+//     married: false
+//   };
+  
+//   for (let key in object) {
+//     if (typeof object[key] === 'string') {
+//       console.log(object[key]);
+//     }
+//   }
+// xndir 2
+
+// let gumar = 0
+// let object = {
+//     name: `Karen`,
+//     surname: `Abovyan`,
+//     profession: `Developer`,
+//     age: 24,
+//     salary: 420000,
+//     married: false
+//   };
+
+//   for (let key in object) {
+//     if(typeof object[key] === "number"){
+//         gumar = gumar + object[key]
+//     }
+//   }
+//   console.log(gumar);
+
+  // xndir 3
+
+// let object = {
+//     name: `Karen`,
+//     surname: `Abovyan`,
+//     profession: `Developer`,
+//     age: 24,
+//     salary: 420000,
+//     married: false
+//   };
+
+//   let array = Object.values(object);
+//   console.log(array);
+
+  // xndir 4
+
+// let object = {
+//     name: `Karen`,
+//     surname: `Abovyan`,
+//     profession: `Developer`,
+//     age: 24,
+//     salary: 420000,
+//     married: false
+//   };
+
+//   array = Object.keys(object)
+//   console.log(array);
